bb68eeff582d3fedd587d6ab997c0bff21e1442c
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-03-03 22:41:59 +0900

    feat: 수동 테스트 페이지 추가

---
+++ src/app/pages/test/MyTestGround.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { EmptyState } from '../../shared/components/state/EmptyState';
+
+/**
+ * 해당 컴포넌트는 기능 개발한 요소들을 직접 테스트 하기 위한 페이지이다.
+ */
+const MyTestGround = () => {
+  return (
+    <div>
+      <EmptyState />
+    </div>
+  );
+};
+
+export default MyTestGround;
---
+++ src/app/pages/test/TestHomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const TestHomePage = () => {
+  const nav = useNavigate();
+  return (
+    <div>
+      <button onClick={() => nav("/test/my")}>커스텀 테스트</button>
+    </div>
+  );
+};
+
+export default TestHomePage;
